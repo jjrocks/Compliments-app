0337d2e6c1de1e2fe7b84b2e53423171a288509f
JJ Wanda <jjwanda@gmail.com>
2016-03-05 12:45:32 -0500

    Creating a transparent bar

--- app/src/main/java/com/jjwanda/compliments/ComplimentsActivity.java
+++ app/src/main/java/com/jjwanda/compliments/ComplimentsActivity.java
@@ -1,7 +1,9 @@
 package com.jjwanda.compliments;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.jjwanda.complimentscommon.Compliments;
